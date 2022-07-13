f3108eee8a5b7b7692c59dc8b732e3a42e8cec5b
rfinn <rfinn>
2022-07-13 07:48:16 -0400

    adding testing and case handling for normalize_patient

--- inflammation/models.py
+++ inflammation/models.py
@@ -44,3 +44,22 @@ def daily_min(data):
     """
     return np.min(data, axis=0)
 
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative.')
+
+    max_data = np.max(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data/ max_data[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    #print(normalised)
+    return normalised
+
+
